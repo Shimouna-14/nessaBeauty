2685c05b2bafdf3407db4d9ebd58d44a4c4f1180
NDEBY Véronique Chimène <chimene.ndeby@gmail.com>
2023-04-06 21:47:40 +0200

    Image/Presentation

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,87 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import '../styles/style.css'
+import styled from 'styled-components'
+import ImgStone from '../images/3d-stepping-stones-in-the-ocean-at-sunset.jpg'
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  top: -20px;
+  @media screen and (max-width: 600px) {
+    position: initial;
+  }
+`
+
+const Stone = styled.img`
+  height: auto;
+  width: 70%;
+  border-radius: 3px;
+  @media screen and (max-width: 600px) {
+    width: 100%;
+  }
+`
+
+const Presention = styled.section`
+  background-color: #ddccc5;
+  width: 100%;
+  height: 26em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 4em 0;
+  @media screen and (max-width: 1023px) { margin: 2.5em 0; }
+`
+
+const TxtPresentation = styled.div`
+  background-color: #E9E1DA;
+  width: 60%;
+  height: 70%;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  @media screen and (max-width: 1023px) {
+    width: 90%;
+    height: 75%;
+  }
+`
+
+const H1 = styled.h1`
+  @media screen and (max-width: 600px) {width: 85%;}
+`
+
+const H2 = styled.h2`
+  width: 68%;
+  @media screen and (max-width: 600px) {width: 85%;}
+`
+
+const Tarif = styled.section`
+
+`
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <Div>
+          <Stone src={ImgStone} alt='3d stepping stones in the ocean at sunset' />
+        </Div>
+        <Presention>
+          <TxtPresentation>
+            <H1>Esthéticienne à domicile sur la commune Écouen</H1>
+            <H2>Professionnelle de la beauté depuis 2006, forte de mon expérience,
+              je vous propose des soins visage et corps, épilation.</H2>
+          </TxtPresentation>
+        </Presention>
+        <Tarif>
 
+        </Tarif>
       </main>
       <Footer></Footer>
     </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,11 +4,14 @@ import Logo from '../images/logo.png'
 import { useState } from 'react'
 import IconChevron from '../images/chevron-down-solid.webp'
 
-const Head = styled.header`
+const Header = styled.header`
     background-color: #16212E;
-    height: 180px;
+    height: 11em;
     display: flex;
     justify-content: center;
+    @media screen and (max-width: 600px) {
+        height: 9em;
+    }
 `
 
 const Div = styled.div`
@@ -30,6 +33,19 @@ const Chevron = styled.img`
     }
 `
 
+const MenuMobile = styled.div`
+    display: none;
+    @media screen and (max-width: 600px) {
+        display: initial;
+        width: 100%;
+        background-color: #16212E;
+        position: absolute;
+        left: 0;
+        top: 8em;
+        padding: 0 40px;
+        z-index: 2;
+    }
+`
 
 const Menu = styled.div`
     display: flex;
@@ -39,15 +55,12 @@ const Menu = styled.div`
     }
 `
 
-const MenuMobile = styled.div`
+const Img = styled.img`
+    height: 8em;
+    width: auto;
     @media screen and (max-width: 600px) {
-        flex-direction: column;
-        width: 100%;
-        background-color: #16212E;
-        position: absolute;
-        left: 0;
-        top: 180px;
-        padding: 0 40px;
+        height: 6em;
+        width: auto;
     }
 `
 
@@ -58,11 +71,11 @@ const Margin = styled.p`
     }
 `
 
-function Header() {
+function Headerer() {
     const [isOpen, setIsOpen] = useState(true)
 
     return (
-        <Head>
+        <Header>
             <Div>
                 <nav>
                     {isOpen ? (
@@ -73,22 +86,22 @@ function Header() {
                         <>
                             <Chevron src={IconChevron} alt="Icon chevron down solid" onClick={() => setIsOpen(true)} />
                             <MenuMobile>
-                                <p className='white'>Accueil</p>
-                                <p className='white'>Services</p>
-                                <p className='white'>Prestations</p>
+                                <p className='beige'>Accueil</p>
+                                <p className='beige'>Services</p>
+                                <p className='beige'>Prestations</p>
                             </MenuMobile>
                         </>
                     )}
                     <Menu>
-                        <p className='white'>Accueil</p>
-                        <Margin className='white'>Services</Margin>
-                        <p className='white'>Prestations</p>
+                        <p className='beige'>Accueil</p>
+                        <Margin className='beige'>Services</Margin>
+                        <p className='beige'>Prestations</p>
                     </Menu>
                 </nav>
-                <img className='logo' src={Logo} alt='Logo Nessa Beauty' />
+                <Img className='logo' src={Logo} alt='Logo Nessa Beauty' />
             </Div>
-        </Head>
+        </Header>
     )
 }
 
-export default Header;
+export default Headerer;
